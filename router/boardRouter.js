f0ddb1e6500d79ce6a523a4968bfe26dcc32f848
yi-barrack <barrack2@DESKTOP-446HG5I>
2023-07-04 17:56:21 +0900

    댓글 기능 구현, 댓글 삭제 미구현

--- router/boardRouter.js
+++ router/boardRouter.js
@@ -4,6 +4,7 @@ const mysql = require('mysql');
 const path = require('path');
 const editRouter = require('./edit.API');
 const deleteRouter = require('./delete.API');
+const commentRouter = require('./commentRouter');
 
 const pool = mysql.createPool({
     host: 'localhost',
@@ -15,6 +16,7 @@ const pool = mysql.createPool({
 
 router.use('/delete', deleteRouter);
 router.use('/edit', editRouter);
+router.use('/comment', commentRouter);
 
 router.get('/new', function (req, res) {
     res.sendFile(path.join(__dirname, '../for_users/create.html'));
@@ -33,9 +35,13 @@ router.get('/:id', function (req, res) {
                 // 수정 버튼 추가
                 // 새로운 페이지에서 제목, 내용, 작성자를 테이블 형태로 보여줍니다.
                 var postPage = '<style>\
-                table { border - collapse: collapse; } th, td { border: 1px solid black; padding: 8px; width: 300px;}\
+                table { border-collapse: collapse; } th, td { border: 1px solid black; padding: 8px; width: 300px;}\
                 #detail {height : 300px}\
-                </style > ';
+                strong { font-size: 20px; }\
+                li { margin-bottom: 10px; }\
+                li > strong {margin-right:20px;}\
+                </style>';
+                
                 if (req.session.username === post.author) { //자신이 작성자일 경우에만 기능 사용 가능
                     // 수정 버튼
                     postPage += '<button onclick="location.href=\'/board/edit/' + postId + '\'">수정</button>';
@@ -49,8 +55,31 @@ router.get('/:id', function (req, res) {
                 postPage += '<tr><th>제목</th><td>' + post.title + '</td><th>작성자</th><td>' + post.author + '</td></tr>';
                 postPage += '<tr><th id = "detail">내용</th><td colspan="3" id = "detail">' + post.content + '</td></tr>';
                 postPage += '</table>';
-                res.send(postPage);
 
+                // 댓글 기능 구현
+                pool.query('SELECT * FROM comments WHERE post_id = ?', [postId], function (error, results, fields) {
+                    if (error) throw error;
+                    postPage += '<h2>댓글</h2>';
+                    postPage += '<ul>';
+                    for (var i = 0; i < results.length; i++) {
+                        var comment = results[i];
+                        var date = new Date(comment.created_at);
+                        var formattedDate = date.getFullYear() + '-' + (date.getMonth() + 1).toString().padStart(2, '0') + '-' + date.getDate().toString().padStart(2, '0') + ' ' + date.getHours().toString().padStart(2, '0') + ':' + date.getMinutes().toString().padStart(2, '0');
+                        postPage += '<li>';
+                        postPage += '<strong>' + comment.author + '</strong> ';
+                        postPage += formattedDate;
+                        postPage += '<br>';
+                        postPage += comment.content;
+                        postPage += '</li>';
+                    }
+                    postPage += '</ul>';
+                    postPage += '<hr>';
+                    postPage += '<form action="./comment" method="post">';
+                    postPage += '<input type="hidden" name="postId" value="' + postId + '">';
+                    postPage += '<input type="text" id="content" name="content" placeholder="댓글을 입력하세요">';
+                    postPage += '</form>';
+                    res.send(postPage); // 비동기라 안에 둬야 데이터베이스에서 가져온 후 작동
+                })
             } else {
                 res.status(404).send('게시물 없음');
             }
@@ -71,7 +100,7 @@ router.post('/new', function (req, res) {
         if (error) throw error;
         res.redirect('/'); // 게시글 작성 후 메인 페이지로 리다이렉트
     });
-    
+
 });
 
 module.exports = router;
---
+++ router/commentRouter.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const router = express.Router();
+const mysql = require('mysql');
+const path = require('path');
+
+const pool = mysql.createPool({
+    host: 'localhost',
+    port: '3306',
+    user: 'barrack',
+    password: process.env.MYSQL_PASSWORD,
+    database: 'user_db'
+});
+
+router.post('/', function (req, res) {
+    // post로 온 데이터들을 변수로 지정
+    var content = req.body.content;
+    var author = req.session.username;
+    var postId = req.body.postId;
+
+    // 데이터베이스에 저장하기 위한 쿼리
+    pool.query('INSERT INTO comments (post_id, content, author) VALUES (?, ?, ?)', [postId, content, author], function (error, results, fields) {
+        if (error) throw error;
+        res.redirect('/board/' + postId);
+    });
+});
+
+module.exports = router;
