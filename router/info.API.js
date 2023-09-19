f5bf99f0632bcd32a5b6eadfff028b048b3c8d8a
imdavid12@naver.com <imdavid12@naver.com>
2023-09-19 14:27:39 +0000

    등원정보 db 연결

--- router/info.API.js
+++ router/info.API.js
@@ -9,37 +9,45 @@ router.get('/', function (req, res) {
     pool.query('SELECT * FROM students', function (err, results) {
         if (err) {
             console.log(err);
+            return;
         }
-        res.json(results);
-    }
-    );
-    
-    res.send(`
-    <!DOCTYPE html>
-    <html lang="ko">
 
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    </head>
+        let html = `
+        <!DOCTYPE html>
+        <html lang="ko">
 
-    <body>
-    <h1>등원정보</h1>
-    <table>
-    <tr>
-    <th>이름</th>
-    <th>등원시간</th>
-    <th>등원여부</th>
-    </tr>
-    <tr>
-    <td>김민수</td>
-    <td>08:30</td>
-    <td>등원</td>
-    </tr>
-    </table>
-    `
-    );
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        </head>
+
+        <body>
+        <h1>등원정보</h1>
+        <table>
+        <tr>
+        <th>이름</th>
+        <th>등원시간</th>
+        <th>등원여부</th>
+        </tr>
+        `;
+
+        console.log(results);
+
+
+        results.forEach(result => {
+            html += `
+                <tr>
+                    <td>${result.student_name}</td>
+                    <td>${result.student_time}</td>
+                    <td>${result.attendant}</td>
+                </tr>
+            `
+        });
+
+        html += `</table></body></html>`;
+        res.send(html);
+    });
 });
 
 
