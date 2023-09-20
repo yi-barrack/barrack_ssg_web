adf60cd408ff52102fc89dc093e818cb1c949b3f
yi-barrack <imdavid12@naver.com>
2023-09-20 03:44:16 +0000

    등원정보 포멧 수정 및 버튼 생성

--- router/info.API.js
+++ router/info.API.js
@@ -4,6 +4,20 @@ const path = require('path');
 const xssFilters = require('xss-filters');
 const pool = require('./students_db.js');
 
+function formatDate(dateString) {
+    const options = {
+        year: '2-digit',
+        month: '2-digit',
+        day: '2-digit',
+        weekday: 'short',
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+        hour12: false,
+    };
+
+    return new Date(dateString).toLocaleDateString('ko-KR', options);
+}
 
 router.get('/', function (req, res) {
     pool.query('SELECT * FROM students', function (err, results) {
@@ -24,33 +38,34 @@ router.get('/', function (req, res) {
 
         <body>
         <h1>등원정보</h1>
-        <table>
+        <table border="1">
         <tr>
         <th>이름</th>
         <th>등원시간</th>
         <th>등원여부</th>
+        <th>버튼</th>
         </tr>
         `;
 
         console.log(results);
 
-
         results.forEach(result => {
+            const formattedDate = formatDate(result.student_time);
             html += `
                 <tr>
                     <td>${result.student_name}</td>
-                    <td>${result.student_time}</td>
+                    <td>${formattedDate}</td>
                     <td>${result.attendant}</td>
+                    <td><button link="/info/attendant">등원</button>
+                    <button link="/info/attendant">하원</button>
+                    <button link="/info/attendant">결석</button></td>
                 </tr>
-            `
+            `;
         });
 
         html += `</table></body></html>`;
         res.send(html);
     });
 });
-
-
-
 
 module.exports = router;
